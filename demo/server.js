2132a567656d0e588482e1f7adb1824969fcb6d6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-11 13:27:25 -0400

    feat(demo): started demo server code

---
+++ demo/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// demo server for showing CSRF login
+var app = require('express')();
+app.use(require('morgan')('dev'));
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+app.use(session({
+  name: 'server-session-cookie-id',
+  secret: 'my express secret',
+  saveUninitialized: true,
+  resave: true,
+  store: new FileStore()
+}));
+
+app.use(function printSession(req, res, next) {
+  console.log('req.session', req.session);
+  return next();
+});
+
+app.get('/', function (req, res) {
+  res.send('hi there');
+});
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
