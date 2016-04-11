677bd1549ef9bcbf7343851164f9d15a9a721f25
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-11 15:13:21 -0400

    chore(logout): added log out

--- demo/app.js
+++ demo/app.js
@@ -2,6 +2,9 @@
 
 var app = require('express')();
 var helmet = require('helmet');
+var bodyParser = require('body-parser');
+var parseForm = bodyParser.urlencoded({ extended: false })
+
 app.use(helmet())
 app.use(require('morgan')('dev'));
 var session = require('express-session');
@@ -20,10 +23,40 @@ app.use(session({
 }));
 
 var csurf = require('csurf');
-var csrfProtection = csurf();
+var csrfProtection = csurf({
+  value: function getCSRF (req) {
+    console.log('returning csrf token from the request');
+    console.log(req.body);
+    return req.body.csrfmiddlewaretoken;
+  }
+});
+
+function send403(res) {
+  return res.status(403).send('User is not authorized');
+}
+
 app.get('/login', csrfProtection, function(req, res) {
   console.log('csrf token', req.csrfToken());
   res.render('login', { csrfToken: req.csrfToken() });
+});
+
+app.post('/login', parseForm, csrfProtection, function (req, res) {
+  console.log('login post', req.body);
+  if (req.body.email === 'user@company.com' &&
+    req.body.password === 'test') {
+    console.log('successfully logged in user', req.body.email);
+    req.session.user = req.body.email;
+    return res.send('logged in');
+  } else {
+    console.log('failed login for user', req.body.email);
+    return res.redirect('/login');
+  }
+});
+
+app.get('/logout', isLoggedIn, csrfProtection, function(req, res) {
+  console.log('logout', req.session.user);
+  req.session.destroy();
+  res.redirect('/login');
 });
 
 app.use(function printSession(req, res, next) {
@@ -43,7 +76,24 @@ app.get('/', function (req, res) {
   // res.send('Hi there, session views count ' + req.session.views);
   res.send({
     text: 'hi there',
-    views: req.session.views
+    views: req.session.views,
+    user: req.session.user
+  });
+});
+
+// a protected api method
+function isLoggedIn (req, res, next) {
+  if (req.session.user) {
+    console.log('has user');
+    return next();
+  }
+  return send403(res);
+}
+app.get('/api', isLoggedIn, function (req, res) {
+  res.send({
+    text: 'protected api call',
+    username: req.session.user,
+    ok: true
   });
 });
 
