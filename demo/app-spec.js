b7770fc80ce64b8f9041385af674f04eda980487
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-11 15:19:59 -0400

    chore(test): testing no login without CSRF token

--- demo/app-spec.js
+++ demo/app-spec.js
@@ -4,11 +4,40 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', function(){
-  it('respond with json', function(done){
+  it('responds with json', function(done){
     request(app)
       .get('/')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
+  })
+
+  it('has session counter', function(done){
+    request(app)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        text: 'hi there',
+        views: 1
+      })
+      .expect(200, done);
+  })
+})
+
+describe('login', function () {
+  it('has login page', function(done){
+    request(app)
+      .get('/login')
+      .expect('Content-Type', /html/)
+      .expect(200, done);
+  })
+
+  it('cannot login without CSRF token', function (done) {
+    request(app)
+      .post('/login')
+      .field('email', 'user@company.com')
+      .field('password', 'test')
+      .expect(403, done)
   })
 })
